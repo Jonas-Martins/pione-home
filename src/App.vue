f10444f569769d0954f416f3c0e5454563cadfcf
Jonas Martins Linux <jonasblz@outlook.com>
2022-08-26 10:52:33 -0300

    Adicionando configurações do Vuetify e configurações extras

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,24 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <v-app>
+    <MyHome></MyHome>
+  </v-app>
 </template>
 
+<script>
+import MyHome from "./views/MyHome.vue";
+export default {
+  components: {
+    MyHome,
+  },
+  name: "App",
+
+  data: () => ({
+    //
+  }),
+};
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+* {
+  font-family: "Secular One", sans-serif;
 }
 </style>
