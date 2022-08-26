4cfb78b39a9eb92d92eee62a2aa9c1ec0eeffc47
Jonas Martins Linux <jonasblz@outlook.com>
2022-08-26 10:49:34 -0300

    Adiciona o MenuTop - Adicionando o menu do top da página inicial

---
+++ src/components/TopMenu.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav class="d-flex justify-space-around align-center">
+    <div>
+      <img width="50px" src="@/assets/logos/Pione2Dark.svg" />
+    </div>
+    <div>
+      <ul class="d-flex" style="list-style: none">
+        <li style="cursor: pointer; font-size: large;" class="mx-5" v-for="(list, i) in menuList" :key="i">
+          {{ list.name }}
+        </li>
+      </ul>
+    </div>
+    <div>
+      <v-btn color="black" dark large> Get Started </v-btn>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menuList: [
+        { name: "Home", link: "" },
+        { name: "Price", link: "" },
+        { name: "Contact", link: "" },
+        { name: "About", link: "" },
+        { name: "Jobs", link: "" },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>>
