6193b5a7f3f09e84bb2ebb5afee3556ff9de3cc9
Jonas Martins Linux <jonasblz@outlook.com>
2022-08-26 10:50:10 -0300

    Adiciona o primeiro bloco do site - Adicionando o primeiro bloco do site, onde contém algumas informações sobre a empresa

---
+++ src/components/ApresentationHome.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-row class="justify-center">
+    <v-col class="d-flex flex-column justify-center align-center" cols="5">
+      <b>
+        <span style="font-size: 3.5rem;">
+          Buy, Sell & Swap Digital Assets<span style="color: purple">.</span>
+        </span>
+      </b>
+
+      <span style="text-align: justify;">
+        Order prices are locked in at the time of placement and our
+        non-custodial approach allows for complete control of your Assets <br>
+      <b>Place on order in 5 minutes!</b>
+      </span>
+      <div class="mt-16 d-flex justify-center">
+        <v-btn class="mx-5" color="purple" dark large> Get Started </v-btn>
+        <v-btn class="mx-5" color="purple" text large style="text-decoration: underline;"> How it Works </v-btn>
+      </div>
+    </v-col>
+    <v-col style="height: 100%" cols="5" class="d-flex justify-end">
+      <img height="100%" src="@/assets/characters/homeChar.svg" />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
+
